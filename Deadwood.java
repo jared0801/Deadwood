d044b9f81ae05e248f006e90ba71aa8da1b4f89d
stewar69 <stewar69@wwu.edu>
2018-11-07 15:18:43 -0800

    created xmlparse file

--- Deadwood.java
+++ Deadwood.java
@@ -1,6 +1,6 @@
 public class Deadwood {
     public static void main(String args[]) {
-        System.out.println("Hello Deadwood!");
+
         return;
     }
 }
---
+++ XMLParse.java
@@ -0,0 +1,28 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class XMLParse {
+
+    public Document getDocFromFile(String filename) throws ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = null;
+
+        try {
+            doc = db.parse(filename);
+
+        } catch (Exception ex) {
+            System.out.println("XML parse failure");
+            ex.printStackTrace();
+        }
+        return doc;
+    }
+}
